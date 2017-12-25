8f689304fb176311678accd4d376f40009771b3e
张一丁 <zhangyiding@corp.netease.com>
2017-12-25 15:47:46 +0800

    init
    = =

---
+++ index.js
@@ -0,0 +1,21 @@
+(function() {
+  let info = document.querySelectorAll(".moduleoutput tbody")[0];
+  let nodes = document.querySelectorAll(".moduleoutput tbody")[1];
+  function get(tbody, row, col) {
+    let trs = tbody.querySelectorAll("tr");
+    return trs[row].querySelectorAll("td")[col].innerText;
+  }
+  let server_port = get(info, 0, 1);
+  let password = get(info, 0, 3);
+
+  let servers = Array.from(nodes.querySelectorAll("tr")).map(tr => {
+    let tds = tr.querySelectorAll("td");
+    return {
+      remarks: tds[1].innerText,
+      server: tds[3].innerText,
+      server_port,
+      password
+    };
+  });
+  console.log(JSON.stringify(servers));
+})();
